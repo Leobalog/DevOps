baff532b7389280650c2f370d96f8512a4edd02e
Leobalog <bale22by@student.ju.se>
2024-09-08 12:49:34 +0200

    Removed logger from Calculator_helper

--- BE/calculator_helper.py
+++ BE/calculator_helper.py
@@ -1,5 +1,3 @@
-import logger
-
 class CalculatorHelper():
     log_properties = {
         'custom_dimensions': {
@@ -22,7 +20,6 @@ class CalculatorHelper():
             admin = self.User('admin','test1234')
             self._user_list.append(admin)
             self._is_initialized = True
-            self.logger = logger.get_logger(__name__)
 
     class User():
         def __init__(self, username, password):
@@ -33,19 +30,15 @@ class CalculatorHelper():
             return f"User(username={self.username}, password={self.password})"
 
     def add(self, a, b):
-        self.logger.debug(f'The calculator has read the data using the add function "{a} + {b}" and has calculated the result.', extra=self.log_properties)
         return a + b
 
     def subtract(self, a, b):
-        self.logger.debug(f'The calculator has read the data using the sub function "{a} - {b}" and has calculated the result.', extra=self.log_properties)
         return a - b
 
     def multiply(self, a, b):
-        self.logger.debug(f'The calculator has read the data using the multiplication function "{a} * {b}" and has calculated the result.', extra=self.log_properties)
         return a * b
 
     def divide(self, a, b):
-        self.logger.debug(f'The calculator has read the data using the div function "{a} / {b}" and has calculated the result.', extra=self.log_properties)
         return a / b
 
     def register_user(self, username, password):
@@ -54,21 +47,18 @@ class CalculatorHelper():
                 return None
         user = self.User(username, password)
         self._user_list.append(user)
-        self.logger.debug(f'User "{username}" has been successfully registered.', extra=self.log_properties)
         return username
 
     def login(self, username, password):
         for user in self._user_list:
             if(user.username == username and user.password == password):
                 self._current_user = user
-                self.logger.debug(f'User "{username}" has successfully logged in.', extra=self.log_properties)
                 return username
         return None
 
     def logout(self):
         user = self._current_user
         self._current_user = None
-        self.logger.debug(f'User "{user}" has successfully logged out.', extra=self.log_properties)
         return user
 
     def get_current_user(self):
