be91505c6c3aae030c59256fd72fc76f82f83c8d
Leobalog <bale22by@student.ju.se>
2024-09-08 17:30:24 +0200

    Pushing all of my new laborations 2 files to main (hope this works)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,10 @@
 import requests
+from calculator_client.client import Client
+from calculator_client.api.actions import calculate
+from calculator_client.models.calculation import Calculation
+from calculator_client.models.opertions import Opertions
+from calculator_client.models import ResultResponse
+
 
 class TestCalculatorAPI():
     def test_calculate_add(self):
@@ -10,3 +16,58 @@ class TestCalculatorAPI():
             "operand2": 1
         }
         response = requests.post(url, json=payload)
+
+    def test_generated_code_add(self):
+        client = Client(base_url="http://localhost:5000")
+        response = calculate.sync(client = client, body = Calculation(Opertions.ADD, operand1=1,operand2=1))
+        assert isinstance(response, ResultResponse)
+        assert response.result == 2
+
+    def test_calculate_subtract(self):
+        url = "http://localhost:5000/calculate"
+
+        payload = {
+            "operation": "subtract" ,
+            "operand1": 1,
+            "operand2": 1
+        }
+        response = requests.post(url, json=payload)
+
+    def test_generated_code_subtract(self):
+        client = Client(base_url="http://localhost:5000")
+        response = calculate.sync(client = client, body = Calculation(Opertions.SUBTRACT, operand1=1,operand2=1))
+        assert isinstance(response, ResultResponse)
+        assert response.result == 0
+
+    def test_calculate_divide(self):
+        url = "http://localhost:5000/calculate"
+
+        payload = {
+            "operation": "divide" ,
+            "operand1": 1,
+            "operand2": 1
+        }
+        response = requests.post(url, json=payload)
+
+    def test_generated_code_divide(self):
+        client = Client(base_url="http://localhost:5000")
+        response = calculate.sync(client = client, body = Calculation(Opertions.DIVIDE, operand1=1,operand2=1))
+        assert isinstance(response, ResultResponse)
+        assert response.result == 1
+
+    def test_calculate_multiply(self):
+        url = "http://localhost:5000/calculate"
+
+        payload = {
+            "operation": "multiply" ,
+            "operand1": 1,
+            "operand2": 1
+        }
+        response = requests.post(url, json=payload)
+
+    def test_generated_code_multiply(self):
+        client = Client(base_url="http://localhost:5000")
+        response = calculate.sync(client = client, body = Calculation(Opertions.MULTIPLY, operand1=1,operand2=1))
+        assert isinstance(response, ResultResponse)
+        assert response.result == 1
+
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,4 +1,6 @@
 from BE.BE.calculator_helper import CalculatorHelper
+import pytest
+
 
 class Testcalculator():
     def test_add(self):
@@ -32,6 +34,14 @@ class Testcalculator():
         value = calculator.multiply(1,1)
         # Assert
         assert value == 1
+    
+    def setup_method(self, method):
+        self.calculator = CalculatorHelper()
+
+
+    def teardown_method(self, method):
+        del self.calculator
+    
 
     def test_divide(self):
         # Arrange
@@ -40,3 +50,12 @@ class Testcalculator():
         value = calculator.divide(1,1)
         # Assert
         assert value == 1
+
+    def test_division_by_zero(self):
+        # Arrange
+        calculator = CalculatorHelper()
+        # Action
+        with pytest.raises(ZeroDivisionError) as exinfo:
+            value = calculator.divide(4,0)
+        # Assert
+        assert "division by zero" in str(exinfo.value), "Expected exception"
