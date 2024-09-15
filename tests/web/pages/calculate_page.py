1ea1cf47f18462823bb3e669d2dfb8d793a06e61
Leobalog <bale22by@student.ju.se>
2024-09-15 14:37:21 +0200

    test

---
+++ tests/web/pages/calculate_page.py
@@ -0,0 +1,31 @@
+from tests.web.pages.page_base import PageBase
+from tests.web.helpers.element import Element
+from munch import munchify
+
+
+class CalculatePage(PageBase):
+    def __init__(self, driver):
+        PageBase.__init__(self, driver = driver)
+
+        self.page_elements = {
+            'key1': Element('//button[@id="key-1"]', self),
+            'key2': Element('//button[@id="key-2"]', self),
+            'key3': Element('//button[@id="key-3"]', self),
+            'key4': Element('//button[@id="key-4"]', self),
+            'key5': Element('//button[@id="key-5"]', self),
+            'key6': Element('//button[@id="key-6"]', self),
+            'key7': Element('//button[@id="key-7"]', self),
+            'key8': Element('//button[@id="key-8"]', self),
+            'key9': Element('//button[@id="key-9"]', self),
+            'key0': Element('//button[@id="key-0"]', self),
+            'keydecimal': Element('//button[@id="key-decimal"]', self),
+            'keyadd': Element('//button[@id="key-add"]', self),
+            'keysubtract': Element('//button[@id="key-subtract"]', self),
+            'keymultiply': Element('//button[@id="key-multiply"]', self),
+            'keydivide': Element('//button[@id="key-divide"]', self),
+            'keyequals': Element('//button[@id="key-equals"]', self),
+            'keyclear': Element('//button[@id="key-clear"]', self),
+            'screen': Element('//input[@id="calculator-screen"]', self),
+        }
+
+        self.elements = munchify(self.page_elements)
--- tests/web/web_test.py
+++ tests/web/web_test.py
@@ -3,6 +3,7 @@ from time import sleep
 from tests.web.test_base import WebBase
 from tests.web.pages.login_page import LoginPage
 from tests.web.pages.login_page import RegisterPage
+from tests.web.pages.calculate_page import CalculatePage
 from assertpy import assert_that
 
 
@@ -26,3 +27,58 @@ class TestReg(WebBase):
         assert_that(RegisterPage(self.driver).elements.username_registered.text).is_equal_to('User already exists!')
         # assert_that(RegisterPage(self.driver).elements.username_logged_in.text).is_equal_to(unique_username)
         a = 2
+
+class Testcalculator(WebBase):
+    def test_add(self):
+        LoginPage(self.driver).elements.username.set('admin')
+        LoginPage(self.driver).elements.password.set('test1234')
+        LoginPage(self.driver).elements.login.click()
+        CalculatePage(self.driver).elements.key1.click()
+        CalculatePage(self.driver).elements.keyadd.click()
+        CalculatePage(self.driver).elements.key2.click()
+        CalculatePage(self.driver).elements.keyequals.click()
+        assert_that(CalculatePage(self.driver).elements.screen.value).is_equal_to('3')
+
+        a = 3
+ 
+class Testcalculator(WebBase):
+    def test_sub(self):
+        LoginPage(self.driver).elements.username.set('admin')
+        LoginPage(self.driver).elements.password.set('test1234')
+        LoginPage(self.driver).elements.login.click()
+        CalculatePage(self.driver).elements.key2.click()
+        CalculatePage(self.driver).elements.keysubtract.click()
+        CalculatePage(self.driver).elements.key1.click()
+        CalculatePage(self.driver).elements.keyequals.click()
+        assert_that(CalculatePage(self.driver).elements.screen.value).is_equal_to('1')
+
+        a = 4
+
+
+class Testcalculator(WebBase):
+    def test_multiply(self):
+        LoginPage(self.driver).elements.username.set('admin')
+        LoginPage(self.driver).elements.password.set('test1234')
+        LoginPage(self.driver).elements.login.click()
+        CalculatePage(self.driver).elements.key2.click()
+        CalculatePage(self.driver).elements.keymultiply.click()
+        CalculatePage(self.driver).elements.key1.click()
+        CalculatePage(self.driver).elements.keyequals.click()
+        assert_that(CalculatePage(self.driver).elements.screen.value).is_equal_to('2')
+
+        a = 5
+
+
+class Testcalculator(WebBase):
+    def test_div(self):
+        LoginPage(self.driver).elements.username.set('admin')
+        LoginPage(self.driver).elements.password.set('test1234')
+        LoginPage(self.driver).elements.login.click()
+        CalculatePage(self.driver).elements.key2.click()
+        CalculatePage(self.driver).elements.keydivide.click()
+        CalculatePage(self.driver).elements.key1.click()
+        CalculatePage(self.driver).elements.keyequals.click()
+        assert_that(CalculatePage(self.driver).elements.screen.value).is_equal_to('2')
+
+        a = 6
+
