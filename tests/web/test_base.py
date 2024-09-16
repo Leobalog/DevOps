fe2f53db9adda3b6fbadfbd694672fd1aee0d6de
Leobalog <bale22by@student.ju.se>
2024-09-16 15:45:17 +0200

    Test internal non remote

--- tests/web/test_base.py
+++ tests/web/test_base.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from tests.web.pages.login_page import LoginPage
+from time import sleep
+
+
 
 class WebBase:
 
@@ -9,21 +13,28 @@ class WebBase:
         """ Setup to run once
             Initiatiung some common parameters
         """
-        cls.app_url = 'http://host.docker.internal:8080/'
-
+        #cls.app_url = 'http://host.docker.internal:8080/' REMOTE!
+        cls.app_url = 'http://localhost:8080/'
     def setup_method(self):
         """ Setup to run before every test
             Initiate a new driver.
         """
+    
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--disable-search-engine-choice-screen")
-        #self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options = chrome_options)
-        self.driver = webdriver.Remote(command_executor = "http://localhost:4444", options=chrome_options)
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options = chrome_options)
+        #self.driver = webdriver.Remote(command_executor = "http://localhost:4444", options=chrome_options) REMOTE!
         self.driver.set_window_size(1920,1080)
         self.driver.get(self.app_url)
+        
 
     def teardown_method(self):
         """ Teardown to run after every test
             Stop the driver
         """
+        if (LoginPage(self.driver).element.logout.find()):
+           LoginPage(self.driver).element.logout.click()
         self.driver.quit()
+        
+        # Ensure that no user is logged in when a test starts
+        
