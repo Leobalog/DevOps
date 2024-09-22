eeeaee39815d4b8a15b96c515a93a226789381f5
Leobalog <bale22by@student.ju.se>
2024-09-22 14:44:59 +0200

    Labb4

--- tests/perf_test/locustfile.py
+++ tests/perf_test/locustfile.py
@@ -1,12 +1,15 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, tag, between
 import json
 
 class CalculatorUser(HttpUser):
 
+    wait_time = between(2,4)
+
     def on_start(self):
         pass
 
-    @task
+    @task(2)
+    @tag("Add")
     def add(self):
         add = {
             "operation": "add",
@@ -17,6 +20,50 @@ class CalculatorUser(HttpUser):
             response_data = json.loads(response.text)
             if not response_data['result'] == 2:
                 response.failure(f"Expected result to be 2 but was {response_data['result']}")
+
+
+    @task
+    @tag("Sub")
+    def subtract(self):
+        sub = {
+            "operation": "subtract",
+            "operand1": 2,
+            "operand2": 1
+        }
+        with self.client.post("/calculate", catch_response=True, name='subtract', json=sub) as response:
+            response_data = json.loads(response.text)
+            if not response_data['result'] == 1:
+                response.failure(f"Expected result to be 1 but was {response_data['result']}")
+
+
+    @task
+    @tag("Multiply")
+    def multiply(self):
+        multiply = {
+            "operation": "multiply",
+            "operand1": 2,
+            "operand2": 1
+        }
+        with self.client.post("/calculate", catch_response=True, name='multiply', json=multiply) as response:
+            response_data = json.loads(response.text)
+            if not response_data['result'] == 2:
+                response.failure(f"Expected result to be 2 but was {response_data['result']}")
+
+
+    @task
+    @tag("Divide")
+    def divide(self):
+        divide = {
+            "operation": "divide",
+            "operand1": 2,
+            "operand2": 1
+        }
+        with self.client.post("/calculate", catch_response=True, name='divide', json=divide) as response:
+            response_data = json.loads(response.text)
+            if not response_data['result'] == 2:
+                response.failure(f"Expected result to be 2 but was {response_data['result']}")
+
+
 
 if __name__ == "__main__":
     from locust import run_single_user
